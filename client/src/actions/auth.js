807f2b9879e8e1b2f9d5304ea83f24bb4ad5a648
Will Koste <willkoste@gmail.com>
2019-11-13 11:14:08 -0500

    protect routes depending on state

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -98,6 +98,6 @@ export const login = (email, password) => async dispatch => {
 
 
 // Logout & Clear Profile
-export const logout = () =>dispatch => {
+export const logout = () => dispatch => {
   dispatch({type: LOGOUT});
 }
