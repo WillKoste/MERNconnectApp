35f82ac6a84d42a5afa3469bfe8695b8b32dc0f9
Will Koste <willkoste@gmail.com>
2019-11-18 10:04:11 -0500

    Profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,6 +7,7 @@ import {logout} from '../../actions/auth';
 export const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
   const authLinks = (
     <ul>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/dashboard"><i className="fas fa-user"></i>{' '} <span className="hide-sm">Dashboard</span></Link></li>
       <li><a onClick={logout} href="#!"><i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Logout</span></a></li>
     </ul>
@@ -14,7 +15,7 @@ export const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
 
   const guestLinks = (
     <ul>
-      <li><a href="#!">Developers</a></li>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
